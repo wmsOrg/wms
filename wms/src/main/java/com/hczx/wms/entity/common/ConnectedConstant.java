d67afe9459131debc4176959b8e3241527138bb4
listen <2998694067@qq.com>
2020-09-24 12:02:14 +0800

    方案详情修改

---
+++ wms/src/main/java/com/hczx/wms/entity/common/ConnectedConstant.java
@@ -0,0 +1,9 @@
+package com.hczx.wms.entity.common;
+
+public class ConnectedConstant {
+
+    public static String Serial_RS232 = "COM9:115200";
+    public static String TCPClient = "192.168.1.168:8160";
+    public static String Serial_RS485 = "COM9:115200:1";
+
+}
